9f1e858f971424a06e3500ed388460733514ef41
Henrique Hadermeck <henriquezanferrari@gmail.com>
2019-07-08 12:20:41 -0300

    Firebase adicionado ao projeto

---
+++ Auth/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
+class App extends Component {
+
+    componentDidMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBDyq9dys0a1tG2x84EzGiLtpQxVPoHRnU',
+            authDomain: 'auth-2ee45.firebaseapp.com',
+            databaseURL: 'https://auth-2ee45.firebaseio.com',
+            projectId: 'auth-2ee45',
+            storageBucket: '',
+            messagingSenderId: '159221081905',
+            appId: '1:159221081905:web:53c8e69c05cb8a86'
+        })
+    }
+
+    render() {
+        return (
+            <View>
+                <Header headerText="Auth" />
+                <Text>App</Text>
+            </View>
+        )
+    }
+}
+
+export default App;
